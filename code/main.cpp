a9fd2ef670434f5407c3646142a608fe6693ac77
Spyros Papanastasiou <spyros_ladon@zoho.eu>
2025-02-19 20:10:07 +0200

    bup

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,12 @@
 #include "platform.h"
-#include "Sil9022A.h"	// Sil9022A abstraction class
+#include "Sil9022A.h"	// Sil9022A{}
+#include "LM75BD.h"		// LM75BD{}
 #include <cstdio>		// printf()
+
+extern "C"
+{
+	#include "sleep.h"	// usleep()
+}
 
 int main()
 {
@@ -11,6 +17,14 @@ int main()
 	//  Initialize/ Power up the HDMI transmitter
 	// ===========================================
 	Sil9022A sil;
+
+	LM75BD lm;
+
+	while (true)
+	{
+		lm.read({0x00}, 1);
+		usleep(1300000);
+	}
 
 	printf("Successfully ran Hellow World application\r\n");
 	cleanup_platform();
